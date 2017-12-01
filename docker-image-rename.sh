21d8bda0d3b043b82cf0d26bcafe5ac61b0cdfe2
吴云强 <yunqiang.wu@hand-china.com>
2017-12-01 21:50:03 +0800

    update2

--- docker-image-rename.sh
+++ docker-image-rename.sh
@@ -9,8 +9,15 @@
 
 DOCKER_TO_PROJECT=gcr.io/google_containers
 DOCKER_FROM_PROJECT=registry.saas.hand-china.com/google_containers2
-RENAME_IMAGES=kube-addon-manager:v6.4-beta.2,kubernetes-dashboard-amd64:v1.8.0,pause-amd64:3.0,kube-addon-manager
-# 
+RENAME_IMAGES=$1
+
+if [[ x$RENAME_IMAGES == x ]]; then 
+  export COPY_IMAGES=mysql,redis
+  echo "not set args"
+  exit 1
+fi
+
+# kube-addon-manager:v6.4-beta.2,kubernetes-dashboard-amd64:v1.8.0,pause-amd64:3.0 
 
 
 OLD_IFS="$IFS"
@@ -22,6 +29,9 @@ for s in ${RENAME_IMAGES_TO_ARR[@]}
 do
   docker pull ${DOCKER_FROM_PROJECT}/${s}
   docker tag ${DOCKER_FROM_PROJECT}/${s} ${DOCKER_TO_PROJECT}/${s}
+
+  # docker pull ${DOCKER_FROM_PROJECT}/${s%:*}:latest
+  # docker tag ${DOCKER_FROM_PROJECT}/${s} ${DOCKER_TO_PROJECT}/${s%:*}:latest
 done
 docker images -a 
 
