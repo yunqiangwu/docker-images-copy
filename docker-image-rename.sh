86ecb984be76f7750dfe65cec4b7d73f0b19d6cd
yunqiangwu <842269153@qq.com>
2021-02-07 17:13:38 +0800

    fix: ci

--- docker-image-rename.sh
+++ docker-image-rename.sh
@@ -7,31 +7,54 @@
 # curl -s https://raw.githubusercontent.com/yunqiangwu/docker-images-copy/master/docker-image-rename.sh | sh
 
 
-DOCKER_TO_PROJECT=gcr.io/google_containers
-DOCKER_FROM_PROJECT=registry.saas.hand-china.com/google_containers2
-RENAME_IMAGES=$1
+# DOCKER_REGISTRY_HOST=registry.saas.hand-china.com
+# DOCKER_USER=yunqiang.wu
+# DOCKER_PWD=Wu950429
+# DOCKER_FROM_PROJECT=gcr.io/google_containers
+# DOCKER_TO_PROJECT=registry.saas.hand-china.com/google_containers2
+# COPY_IMAGES
 
-if [[ x$RENAME_IMAGES == x ]]; then 
+if [[ x$COPY_IMAGES == x ]]; then 
+  export COPY_IMAGES=$1
+fi
+
+if [[ x$IMAGES_VERSION == x ]]; then 
+  export IMAGES_VERSION=latest
+fi
+
+if [[ x$COPY_IMAGES == x ]]; then 
   export COPY_IMAGES=mysql,redis
   echo "not set args"
   exit 1
 fi
 
-# kube-addon-manager:v6.4-beta.2,kubernetes-dashboard-amd64:v1.8.0,pause-amd64:3.0 
-
+# docker login $DOCKER_REGISTRY_HOST -u $DOCKER_USER -p $DOCKER_PWD
 
 OLD_IFS="$IFS"
 IFS=","
-RENAME_IMAGES_TO_ARR=($RENAME_IMAGES)
+COPY_IMAGES_TO_ARR=($COPY_IMAGES)
 IFS="$OLD_IFS"
 
-for s in ${RENAME_IMAGES_TO_ARR[@]}
+for s in ${COPY_IMAGES_TO_ARR[@]}
 do
-  docker pull ${DOCKER_FROM_PROJECT}/${s}
-  docker tag ${DOCKER_FROM_PROJECT}/${s} ${DOCKER_TO_PROJECT}/${s}
 
-  # docker pull ${DOCKER_FROM_PROJECT}/${s%:*}:latest
-  # docker tag ${DOCKER_FROM_PROJECT}/${s} ${DOCKER_TO_PROJECT}/${s%:*}:latest
+  export IMAGE_FULL_NAME=${s}
+
+  if [[ ! $IMAGE_FULL_NAME =~ ":" ]]; then 
+
+    export IMAGE_FULL_NAME=${IMAGE_FULL_NAME}:${IMAGES_VERSION}
+
+  fi
+
+  echo ${DOCKER_FROM_PROJECT}/${IMAGE_FULL_NAME} "==>" ${DOCKER_TO_PROJECT}/${IMAGE_FULL_NAME}
+
+  docker pull ${DOCKER_FROM_PROJECT}/${IMAGE_FULL_NAME}
+  docker tag ${DOCKER_FROM_PROJECT}/${IMAGE_FULL_NAME} ${DOCKER_TO_PROJECT}/${IMAGE_FULL_NAME}
+
+  docker tag ${DOCKER_FROM_PROJECT}/${IMAGE_FULL_NAME} ${DOCKER_TO_PROJECT}/${IMAGE_FULL_NAME}
+  docker push ${DOCKER_TO_PROJECT}/${IMAGE_FULL_NAME}
+
 done
-docker images -a 
+
+# docker images -a 
 
